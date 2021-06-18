0c268d5e30e97e5b418a58115dbc50ff3227a1ec
Viktor Kolosov <vitykolosov@gmail.com>
2021-06-18 16:32:24 +0300

    feat: implement start stopwatch function

---
+++ stopwatch/script.js
@@ -0,0 +1,33 @@
+const btns = document.querySelector('.stopwatch__btns')
+const display = document.querySelector('.stopwatch__display');
+const stopwatch = document.querySelector('.stopwatch');
+let milliseconds = 0;
+let timer;
+
+function start() {
+    if (stopwatch.classList.contains('pause')) {
+        stopwatch.classList.remove('pause');
+        clearInterval(timer);
+        timer = setInterval(() => {
+            milliseconds += 10;
+            let time = new Date(milliseconds);
+            display.textContent = `
+            ${(time.getUTCHours() < 10) ? '0' + time.getUTCHours() : time.getUTCHours()} :
+            ${(time.getMinutes() < 10) ? '0' + time.getMinutes() : time.getMinutes()} :
+            ${(time.getSeconds() < 10) ? '0' + time.getSeconds() : time.getSeconds()} :
+            ${(time.getMilliseconds() < 10) ? '00' + time.getMilliseconds() : 
+            (time.getMilliseconds() < 100) ? '0' + time.getMilliseconds() : time.getMilliseconds()}`;
+        }, 10);
+    }
+}
+
+btns.addEventListener('click', (e) => {
+    switch (e.target.textContent) {
+        case 'Start': start();
+            break;
+        case 'Stop':
+            break;
+        case 'Reset':
+            break;
+    }
+});
